bdd79f5115ac39c36099a1db388346f6fd65939f
skyworker-sinica <luke.yc.yang@gmail.com>
2025-03-19 17:14:28 +0800

    modify dataset class

--- dataset.py
+++ dataset.py
@@ -9,19 +9,19 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.dataloader import default_collate
 
 class PARTNET(Dataset):
-    def __init__(self, split='train'):
+    def __init__(self, root_dir, split='train'):
         super(PARTNET, self).__init__()
         
         assert split in ['train', 'val', 'test']
         self.split = split
-        self.root_dir = your_path     
+        self.root_dir = root_dir
         self.files = os.listdir(self.root_dir)
         self.img_transform = transforms.Compose([
                transforms.ToTensor()])
 
     def __getitem__(self, index):
         path = self.files[index]
-        image = Image.open(os.path.join(self.root_dir, path, "0.png")).convert("RGB")
+        image = Image.open(os.path.join(self.root_dir, path)).convert("RGB")
         image = image.resize((128 , 128))
         image = self.img_transform(image)
         sample = {'image': image}
---
+++ train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List of folders
+evalset=(
+    movi_a_0004_anoMask
+)
+
+DATA_DIR=/home/skyworker/result/4DGS_SlotAttention/shape_of_motion
+OUT_DIR=/home/skyworker/result/4DGS_SlotAttention/slot_4dgs
+
+# Batch run preprocess
+for seq in ${evalset[@]}; do
+    python train.py \
+    --data_dir $DATA_DIR/$seq/images \
+    --output_dir $OUT_DIR/$seq \
+    --batch_size 2 \
+    --num_slots 10 \
+    --num_iterations 3 \
+    --num_workers 0 \
+    --num_epochs 1
+done
